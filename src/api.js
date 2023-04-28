c6185dab4abd84d324dacc65adc967aacda7a609
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2023-04-28 12:51:18 -0400

    code cleanup

--- src/api.js
+++ src/api.js
@@ -1,18 +1,16 @@
 import axios from 'axios';
 
-const searchImages = async () => {
+const searchImages = async (term) => {
     const response = await axios.get('https://api.unsplash.com/search/photos', {
         headers: {
-           Authorization: 'Client-ID 5K4T-fbau_x9ZssGkGrwOIkSi8aU1YdFTE8dfo3kYYw'
+           Authorization: 'Client-ID 5K4T-fbau_x9ZssGkGrwOIkSi8aU1YdFTE8dfo3kYYw',
         },
         params: {
-           query: 'oceans'
-        }
+           query: term,
+        },
     });
 
-    console.log(response);
-
-    return response;
+    return response.data.results;
 };
 
 export default searchImages;
