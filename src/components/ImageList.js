5ef817b0bea5bf1ad8f395ad23c2c0603e3afaaf
Lindsay Senia <92329091+lindsaySenia@users.noreply.github.com>
2023-04-30 16:09:13 -0400

    ImageList receives images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,5 @@
-function ImageList() {
-    return <div>ImageList
-    
-    </div>;
+function ImageList({ images }) {
+    return <div>ImageList: {images.length}</div>;
 };
 
 export default ImageList;
